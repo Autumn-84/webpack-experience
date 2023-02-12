77c7ce19c644b36e938e735d54d11ba95fe85ccb
chuangyu <2937761459@qq.com>
2023-02-12 20:38:29 +0800

    体验

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import a from './a.js';
 import b from './b.js';
 import './index.less';
+import './a.jpg';
+import $ from 'jquery';
 console.log(a, b);
+
+$('<h1>').text('Hello webpack!').appendTo(document.body);
